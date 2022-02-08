60c4cd57a184110100246baf9f9d2555747bb551
sunilkarki <karkisunil1200@gmail.com>
2022-02-08 10:03:53 -0600

    getting data from backend

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,31 +1,29 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      rating: 10,
-      text: "Lorem ipsum dolor sit amet .",
-    },
-    {
-      id: 2,
-      rating: 9,
-      text: " vitae commodi alias ve.",
-    },
-    {
-      id: 3,
-      rating: 8,
-      text: "  est voluptatum ipsa quae.",
-    },
-  ]);
+  const [feedback, setFeedback] = useState([]);
 
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
   });
+
+  useEffect(() => {
+    fetchFeedback();
+  }, []);
+
+  //Fetch Feedback
+
+  const fetchFeedback = async () => {
+    const response = await fetch(
+      `http://localhost:5000/feedback?_sort=id&order=desc`
+    );
+    const data = await response.json();
+    setFeedback(data);
+  };
 
   //set item to be updated
   const editFeedback = (item) => {
